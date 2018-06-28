caa26bdec0b853ba22c750fc77f895666f9dc26f
Colin Nielsen <33375223+colinnielsen@users.noreply.github.com>
2018-06-28 15:48:11 -0600

    eod

--- migrations/20180627100000_myballot.js
+++ migrations/20180627100000_myballot.js
@@ -1,4 +1,4 @@
-exports.up = function (knex) {
+exports.up = function (knex, Promise) {
     return knex.schema.createTable('myballot', table => {
         table.increments('id')
         table.integer('gov')
@@ -14,6 +14,6 @@ exports.up = function (knex) {
     })
 };
 
-exports.down = function (knex) {
+exports.down = function (knex, Promise) {
     return knex.schema.dropTableIfExists('myballot')
 };
